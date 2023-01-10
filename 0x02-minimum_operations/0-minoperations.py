7202f28692944f992cd8d34eb2a0b43c3f26c0cc
KipronohVincent <kipronohvincent43@gmail.com>
2023-01-10 08:55:27 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""
-Minimum Operations
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
 """
 
 
 def minOperations(n):
-    if n == 0:
+    """
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
+
+    Args:
+        n: input value
+        factor_list: List to save the operations
+    Return: the sum of the operations
+    """
+    if n < 2:
         return 0
-    if n == 1:
-        return 1
-    operations = 0
-    while n > 1:
-        operations += 1
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n -= 1
-    return operations + 1
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
